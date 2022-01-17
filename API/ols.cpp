281d9ef4ee26af0ecf21a39f1af7bcaa2bf342c2
Tortilok <domashkin2120@gmail.com>
2022-01-17 03:02:28 +0300

    Добавил тесты и исправил замечания

---
+++ API/ols.cpp
@@ -0,0 +1,149 @@
+﻿#include "ols.h"
+
+ols::ols() : first(nullptr), last(nullptr), size_list(0) {}
+
+ols::~ols()
+{
+	while (this->size() != 0) {
+		node* temp = first->next;
+		if (first != nullptr) {
+			delete first;
+			first = nullptr;
+		}
+		first = temp;
+		size_list -= 1;
+	}
+};
+
+const bool ols::empty() {
+	return first == nullptr;
+};
+
+void ols::add(const int data)
+{
+	node* temp = new node(data);
+	if (empty()) {
+		first = temp;
+		last = temp;
+		size_list += 1;
+		return;
+	}
+	last->next = temp;
+	last = temp;
+	size_list += 1;
+};
+
+const int ols::read(const int index)
+{
+	node* temp = first;
+	for (size_t i = 0; i < index; i++) {
+		temp = temp->next;
+	}
+	int item = temp->data;
+	return item;
+};
+
+void ols::remove(const int index) {
+	if (empty()) {
+		return;
+	}
+	if (index >= size_list)
+		return;
+	else if (index == size_list - 1) {
+		int i = 0;
+		node* temp = first;
+		while (temp != nullptr) {
+			if (i == index - 1) {
+				node* temp2 = temp->next;
+				delete temp2;
+				temp->next = nullptr;
+				break;
+			}
+			temp = temp->next;
+			i += 1;
+		}
+		size_list--;
+	}
+	else if (index == 0) {
+		node* firstNext = first->next;
+		delete first;
+		first = firstNext;
+		size_list--;
+	}
+	else {
+		node* temp = first;
+		int i = 0;
+		while (temp != nullptr) {
+			if (i == index - 1) {
+				node* temp2 = temp->next;
+				node* temp3 = temp2->next;
+				temp->next = temp3;
+				delete temp2;
+				break;
+			}
+			temp = temp->next;
+			i += 1;
+		}
+		size_list--;
+	}
+};
+
+const size_t ols::search(const int data) {
+	node* temp = first;
+	for (size_t i = 1; i < this->size(); i++)
+	{
+		if (temp->data == data)
+			return i;
+		temp = temp->next;
+	}
+
+};
+
+void ols::update(const size_t index, const int data)
+{
+	node* temp = first;
+	size_t num = index;
+	const size_t begin = 1;
+	while (num != begin)
+	{
+		temp = temp->next;
+		num--;
+	}
+	temp->data = data;
+};
+
+const size_t ols::size()
+{
+	return size_list;
+};
+
+
+ostream& operator <<(ostream& out, ols& l) {
+	for (int i = 0; i < l.size(); i++)
+		out << l[i] << " ";
+	return out;
+};
+
+int& ols::operator[] (const int index) {
+	node* cur = first;
+	int i = 0;
+	while (cur != nullptr) {
+		if (i == index) {
+			return cur->data;
+		}
+		else i++;
+		cur = cur->next;
+	}
+};
+
+const int& ols::operator[] (const int index) const {
+	node* cur = first;
+	int i = 0;
+	while (cur != nullptr) {
+		if (i == index) {
+			return cur->data;
+		}
+		else i++;
+		cur = cur->next;
+	}
+};
---
+++ ApiTests/ApiTests.cpp
@@ -0,0 +1,113 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../API/ols.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace ApiTests
+{
+	TEST_CLASS(ApiTests)
+	{
+	public:
+		
+		TEST_METHOD(TestClassCreation)
+		{
+			const auto result = new ols();
+			Assert::IsNotNull(result);
+		};
+
+		TEST_METHOD(TestListEmpty)
+		{
+			size_t expected = 0;
+			ols result;
+			Assert::IsTrue(result.empty());
+			result.add(2);
+			Assert::IsFalse(result.empty());
+		};
+		TEST_METHOD(TestAddItem)
+		{
+			size_t expected = 0;
+			ols result;
+			Assert::AreEqual(expected, result.size());
+			result.add(2);
+			expected = 1;
+			Assert::AreEqual(expected, result.size());
+			Assert::AreEqual(2, result.read(0));
+			result.add(-2);
+			result.add(600);
+			result.add(8);
+			expected = 4;
+			Assert::AreEqual(expected, result.size());
+			Assert::AreEqual(600, result.read(2));
+
+		};
+
+		TEST_METHOD(TestReadItem)
+		{
+			ols result;
+			result.add(-100);
+			Assert::AreEqual(-100, result.read(0));
+			result.add(-2);
+			result.add(70);
+			result.add(8);
+			Assert::AreEqual(70, result.read(2));
+			Assert::AreEqual(8, result.read(3));
+			Assert::AreEqual(-2, result.read(1));
+		};
+		TEST_METHOD(TestRemoveItemStart)
+		{
+			ols result;
+			size_t expected = 3;
+			result.add(100);
+			result.add(200);
+			result.add(300);
+			Assert::AreEqual(expected, result.size());
+			result.remove(0);
+			expected = 2;
+			Assert::AreEqual(expected, result.size());
+		};
+		TEST_METHOD(TestRemoveItemMiddle)
+		{
+			ols result;
+			size_t expected = 4;
+			result.add(100);
+			result.add(-200);
+			result.add(500);
+			result.add(300);
+			Assert::AreEqual(expected, result.size());
+			result.remove(2);
+			expected = 3;
+			Assert::AreEqual(expected, result.size());
+		};
+
+		TEST_METHOD(TestRemoveItemEnd)
+		{
+			ols result;
+			size_t expected = 2;
+			result.add(100);
+			result.add(300);
+			Assert::AreEqual(expected, result.size());
+			result.remove(1);
+			expected = 1;
+			Assert::AreEqual(expected, result.size());
+		};
+
+		TEST_METHOD(TestSearch)
+		{
+			const auto result = new ols();
+			Assert::IsNotNull(result);
+		};
+
+		TEST_METHOD(TestUpdateItem)
+		{
+			const auto result = new ols();
+			Assert::IsNotNull(result);
+		};
+
+		TEST_METHOD(TestSize)
+		{
+			const auto result = new ols();
+			Assert::IsNotNull(result);
+		};
+	};
+}
